22e16b70e925ae9d94dca996d3bf1a56bf04c615
MikkelBJohansen <MikkelBJohansen@icloud.com>
2024-11-04 23:57:16 +0000

    Automated update: 2024-11-04 23:57:16

--- auto_commit.sh
+++ auto_commit.sh
@@ -3,8 +3,11 @@
 # Navigate to your repository directory
 cd /home/pi/danish_data_project
 
+# Pull remote changes
+git pull origin main
+
 # Add changes to git
-git add index.html
+git add index.html graph_html.py auto_commit.sh .gitignore
 
 # Commit changes with a timestamp
 git commit -m "Automated update: $(date '+%Y-%m-%d %H:%M:%S')"
